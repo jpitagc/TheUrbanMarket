62d53c96f8573a82cc6cc7f667f7b258a4a03cd5
Unknown <jpitagc@gmail.com>
2018-11-27 19:17:41 +0100

    27/11/18 Pita
    footer, imagenes de zapas añadido

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,6 @@
-javascript
+/*$(document).ready(function(){
+
+	x = ($(".rectangulo").width()-$(".sneakername").width())/2;
+	alert($(".sneakername").width());
+	$(".sneakersname").css("margin-left",x+ "px");
+});*/
