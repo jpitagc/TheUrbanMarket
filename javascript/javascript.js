0bb58fc6a4ee7a8ce5cf1c087b2bafa495e6e80c
Unknown <jpitagc@gmail.com>
2018-11-28 14:20:03 +0100

    Calendar Homepage

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,1 @@
-/*$(document).ready(function(){
-
-	x = ($(".rectangulo").width()-$(".sneakername").width())/2;
-	alert($(".sneakername").width());
-	$(".sneakersname").css("margin-left",x+ "px");
-});*/
+javascript
