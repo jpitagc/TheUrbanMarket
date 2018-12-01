68e104b908648c6a8bade5a7663230d0fca4c31d
Unknown <jpitagc@gmail.com>
2018-12-01 16:29:00 +0100

    Novedades + Calendar
    La seccion de novedades actualiza con tres imagenes distintas y el calendario medio responde a los cmbios de fecha (aun por completar).

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,50 @@
-javascript
+
+
+var indice= 0;
+var circulo = "circulo";
+var imagenes= new Array(
+      ["../images/megabanner.jpg"],["../images/megabanner1.jpg"],["../images/megabanner2.jpg"]
+	);
+
+onload = function (){
+	changeimage();
+	setInterval(changeimage,8000);
+}
+
+
+function changeimage (){
+	     if (indice >=3) { indice= 0;}
+	     var cir= circulo + indice;
+
+	     var cirant= circulo + (indice -1);
+	     if (cirant == "circulo-1"){ cirant = "circulo2"}
+	     
+
+         document.getElementById("anuncios").src= imagenes [indice];
+
+         document.getElementById(cir).style.backgroundColor = "grey";
+         document.getElementById(cirant).style.backgroundColor = "white";
+         indice++;
+         
+
+}
+
+
+function actualizardia(x){
+    var dia = x.innerHTML;
+    var acro = "TH";
+    if (dia == "1" || dia == "21" || dia == "31") { acro = "ST"}
+    else if( dia == "2" || dia == "22") { acro = "ND"}
+    else if (dia == "3" || dia == "23") { acro = "RD"}
+	document.getElementById("diadelmes").innerHTML = "DECEMBER " + dia + acro;
+    document.getElementById("active").removeAttribute("id");
+    if(x.children[0] === undefined){
+    	x.setAttribute("id","active");
+    	return;
+    }
+    else{
+    	var element = x.children;
+    	
+    	
+    }
+}
